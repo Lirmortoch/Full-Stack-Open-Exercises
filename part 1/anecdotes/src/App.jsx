cff4527c03f521b78fe0f9f06b89a65479b0e64f
Lirmortoch <dima2111323@gmail.com>
2025-09-27 15:45:24 +0300

    finished anecdotes

--- part 1/anecdotes/src/App.jsx
+++ part 1/anecdotes/src/App.jsx
@@ -36,17 +36,37 @@ const App = () => {
 
     setSelected(selected-1);
   }
-
   const handleVote = () => {
-    console.log(votes);
+    const copy = {...votes};
+    copy[selected] = (copy[selected] || 0) + 1;
+    setVotes(copy);
   }
+
+  const pickMostVotedItem = () => {
+    let maxIndex = 0;
+    let max = votes[maxIndex];
+
+    for (const vote in votes) {
+      if (max < votes[vote] || !max) {
+        max = votes[vote];
+        maxIndex = vote;
+      }
+    }
+
+    return max !== undefined ? maxIndex : undefined;
+  }
+  
+  const mostVotedAnecdoteIndex = pickMostVotedItem();
 
   return (
     <main className='anecdotes'>
       <section className='anecdotes__display display'>
         <h2 className='display__title title'>Anecdote of the day</h2>
+
         <p className='display__anecdote'>{anecdotes[selected]}</p>
-        <p className='display__votes-count'>{votes[selected]+''}</p>
+        <p className='display__votes-count'>
+          Has {votes[selected] || 0} vote{votes[selected] > 1 || votes[selected] === undefined  ? 's' : null}
+        </p>
       </section>
 
       <section className='anecdotes__buttons'>
@@ -57,7 +77,14 @@ const App = () => {
 
       <section className='anecdotes__most-votes most-votes'>
         <h2 className='most-votes__title title'>Anecdote with most votes</h2>
-        <p className='most-votes__item'>21</p>
+
+        <p className='most-votes__anecdote'>{anecdotes[mostVotedAnecdoteIndex] || 'Vote for any anecdote'}</p>
+        {anecdotes[mostVotedAnecdoteIndex] && (
+          <p className='most-votes__votes-count'>
+            Has {votes[mostVotedAnecdoteIndex]} vote{votes[mostVotedAnecdoteIndex] > 1 ? 's' : null}
+          </p>
+          )
+        }
       </section>
     </main>
   )
