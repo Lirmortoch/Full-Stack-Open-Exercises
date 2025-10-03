04918494ca99c355648806055d6c3dc8970b9c41
Lirmortoch <dima2111323@gmail.com>
2025-10-03 17:38:52 +0300

    Did exrciese 2.2

--- part 2/courseInfoUpgraded/src/components/Course.jsx
+++ part 2/courseInfoUpgraded/src/components/Course.jsx
@@ -1,15 +1,15 @@
 function Course({ course }) {
-  
+  const totalExercises = course.parts.reduce((acc, item) => acc += item.exercises, 0);
 
   return (
     <section key={course.id}>
         <h2>{course.name}</h2>
-
+        
         <ol>
             {course.parts.map(item => <li key={item.id}>{item.name} {item.exercises}</li>)}
         </ol>
 
-        
+        <p><b>total of {totalExercises} exercises</b></p>
     </section>
   )
 }
