187f7917575c5ec98538b337dd7748a435869376
Lirmortoch <dima2111323@gmail.com>
2025-10-05 12:13:23 +0300

    Added some styles and new input, fixed mistake.

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -32,18 +32,24 @@ function App() {
     <main className='phonebook'>
       <h1 className='phonebook__title title'>Phonebook</h1>
 
-      <form className='phonebook__form phb-form'>
-        <fieldset className='phb-form__fieldset'>
-          <label htmlFor='phb-input' className='phb-form__label'>name</label>
-          <input type='text' className='phb-form__input' id='phb-input' text='phb-input' onChange={handleNameChange} />
-        </fieldset>
-        <fieldset className='phb-form__fieldset phb-form-submit'>
-          <button type="submit" className='phb-form__button' onClick={handleSavePerson} >add</button>
-        </fieldset>
-      </form>
+      <section className='phonebook__form-section form-section'>
+        <h2 className='form-section__title title second-title'>Add new number</h2>
+        
+        <form className='phonebook__form phb-form'>
+          <fieldset className='phb-form__fieldset'>
+            <label htmlFor='phb-input-name' className='phb-form__label'>Name</label>
+            <input type='text' className='phb-form__input' id='phb-input-name' name='phb-input-name' onChange={handleNameChange} placeholder='Enter Name' />
+          </fieldset>
+          <fieldset className='phb-form__fieldset'>
+            <label htmlFor='phb-input-phone' className='phb-form__label'>Number</label>
+            <input type='tel' className='phb-form__input' id='phb-input-phone' name='phb-input-phone' onChange={() => console.log('hi!!!')} placeholder='Enter Phone Number' />
+          </fieldset>
+          <button type="submit" className='phb-form__button' onClick={handleSavePerson} >Add</button>
+        </form>
+      </section>
 
       <section className='phonebook__numbers phb-numbers'>
-        <h2 className='phb-numbers__title title'>Numbers</h2>
+        <h2 className='phb-numbers__title title second-title'>Numbers</h2>
 
         <ul className='phb-numbers__list phb-list'>
           {persons.map(item => <li key={persons.length + 1} className={classNameProp}>{item.name}</li>)}
