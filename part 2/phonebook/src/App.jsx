836e94cf90f02f675f6db5377b1bb4e7d225d354
Lirmortoch <dima2111323@gmail.com>
2025-10-05 11:31:37 +0300

    did 2.6 exercise

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  ]); 
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+  const handleSavePerson = (event) => {
+    event.preventDefault();
+
+    const newPerson = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  }
+
+  const classNameProp = 'phb-list__item'
 
   return (
     <main className='phonebook'>
@@ -14,17 +30,19 @@ function App() {
       <form className='phonebook__form phb-form'>
         <fieldset className='phb-form__fieldset'>
           <label htmlFor='phb-input' className='phb-form__label'>name</label>
-          <input type='text' className='phb-form__input' id='phb-input' text='phb-input' />
+          <input type='text' className='phb-form__input' id='phb-input' text='phb-input' onChange={handleNameChange} />
         </fieldset>
         <fieldset className='phb-form__fieldset phb-form-submit'>
-          <button type="submit" className='phb-form__button'>add</button>
+          <button type="submit" className='phb-form__button' onClick={handleSavePerson} >add</button>
         </fieldset>
       </form>
 
       <section className='phonebook__numbers phb-numbers'>
         <h2 className='phb-numbers__title title'>Numbers</h2>
 
-
+        <ul className='phb-numbers__list phb-list'>
+          {persons.map(item => <li className={classNameProp}>{item.name}</li>)}
+        </ul>
       </section>
       
     </main>
