a2f71ea2484b752c2f04f99e95991b0cb2d87777
Lirmortoch <dima2111323@gmail.com>
2025-10-05 12:20:08 +0300

    did exercise 2.8

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -3,18 +3,23 @@ import './App.css';
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    
   ]); 
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
   }
   const handleSavePerson = (event) => {
     event.preventDefault();
 
     const newPerson = {
       name: newName,
+      phone: newPhone,
     }
 
     if (persons.some(item => item.name === newPerson.name)) {
@@ -34,7 +39,7 @@ function App() {
 
       <section className='phonebook__form-section form-section'>
         <h2 className='form-section__title title second-title'>Add new number</h2>
-        
+
         <form className='phonebook__form phb-form'>
           <fieldset className='phb-form__fieldset'>
             <label htmlFor='phb-input-name' className='phb-form__label'>Name</label>
@@ -42,7 +47,7 @@ function App() {
           </fieldset>
           <fieldset className='phb-form__fieldset'>
             <label htmlFor='phb-input-phone' className='phb-form__label'>Number</label>
-            <input type='tel' className='phb-form__input' id='phb-input-phone' name='phb-input-phone' onChange={() => console.log('hi!!!')} placeholder='Enter Phone Number' />
+            <input type='tel' className='phb-form__input' id='phb-input-phone' name='phb-input-phone' onChange={handlePhoneChange} placeholder='Enter Phone Number' />
           </fieldset>
           <button type="submit" className='phb-form__button' onClick={handleSavePerson} >Add</button>
         </form>
@@ -52,7 +57,7 @@ function App() {
         <h2 className='phb-numbers__title title second-title'>Numbers</h2>
 
         <ul className='phb-numbers__list phb-list'>
-          {persons.map(item => <li key={persons.length + 1} className={classNameProp}>{item.name}</li>)}
+          {persons.map(item => <li key={persons.length + 1} className={classNameProp}>{item.name} - {item.phone}</li>)}
         </ul>
       </section>
       
