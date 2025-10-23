2893db72cb359184fcca1639820e5e3ec96dba3a
Lirmortoch <dima2111323@gmail.com>
2025-10-23 13:41:55 +0300

    Did exercise 3.2

--- part 3/phonebookBackEnd/index.js
+++ part 3/phonebookBackEnd/index.js
@@ -24,11 +24,17 @@ const persons = [
 const express = require('express');
 const app = express();
 
-app.get('/api/persons', (response) => {
-    response.json(persons);
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+app.get('/api/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
