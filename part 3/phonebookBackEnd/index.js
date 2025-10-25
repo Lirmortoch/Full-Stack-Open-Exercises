b9fbe2094576abbd808be7c30222f0a7323f40ea
Lirmortoch <dima2111323@gmail.com>
2025-10-25 18:06:05 +0300

    Did exercise 3.8

--- part 3/phonebookBackEnd/index.js
+++ part 3/phonebookBackEnd/index.js
@@ -32,7 +32,17 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan((tokens, req, res) => {
+  const response = ':method :url :status :res[content-length] - :response-time ms';
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ');
+}));
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
