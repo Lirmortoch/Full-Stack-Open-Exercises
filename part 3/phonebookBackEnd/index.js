d0c23d3713cc77df7d3d63ac3995fc6489290fb5
Lirmortoch <dima2111323@gmail.com>
2025-10-25 17:35:02 +0300

    did exercise 3.7

--- part 3/phonebookBackEnd/index.js
+++ part 3/phonebookBackEnd/index.js
@@ -28,8 +28,11 @@ const generateId = (arr) => {
 }
 
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
