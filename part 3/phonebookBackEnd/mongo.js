5b8412674d94d9742643983502d3b3d2a8161883
Lirmortoch <dima2111323@gmail.com>
2025-11-05 15:52:33 +0300

    Add some code for exercise 3.12

---
+++ part 3/phonebookBackEnd/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const {loadEnvFile, argv} = require('node:process');
+loadEnvFile();
+
+if (process.argv.length < 2) {
+  console.log('Provide user\'s password');
+  process.exit(1);
+}
+
+const password = process.env.PASSWORD_MONGO_DB;
+
+const url = `mongodb+srv://dima2111323_db_user:${password}@cluster0.lzqwpxp.mongodb.net/?appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    number: String,
+});
+const Person = mongoose.model('Person', personSchema);
+
+
