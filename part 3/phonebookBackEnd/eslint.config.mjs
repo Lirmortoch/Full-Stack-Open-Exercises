f03bd611f887a9748c2bb759adfc770f108c966e
Lirmortoch <dima2111323@gmail.com>
2025-11-17 14:01:23 +0300

    add linter and some rules for its

---
+++ part 3/phonebookBackEnd/eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': "off",
+      '@stylistic/js/semi': ['error', 'always'],
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+  {
+    rules: {
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'warn',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+    }
+  },
+]);
