4e4076efeba0c838106a2a51fe66b2636cb266d8
Lirmortoch <dima2111323@gmail.com>
2025-12-23 11:45:43 +0300

    add user form to app

--- part 5/bloglist-frontend-main/src/App.jsx
+++ part 5/bloglist-frontend-main/src/App.jsx
@@ -1,9 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
+import './App.css'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  const usernameRef = useRef()
+  const passwordRef = useRef()
+
+  function userFormSubmit(e) {
+    e.preventDefault()
+  }
+
+  const userForm = () => (
+    <section>
+      <h2>log in to application</h2>
+      <form onSubmit={userFormSubmit}>
+        <fieldset>
+          <label htmlFor='form-username'>username</label>
+          <input type='text' id='form-username' name='form-username' ref={usernameRef} />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='form-password'>password</label>
+          <input type='password' id='form-password' name='form-password' ref={passwordRef} />
+        </fieldset>
+
+        <button type='submit' className='form__btn'>Log In</button>
+      </form>
+    </section>
+  )
+  const blogsList = () => (
+    <section>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </section>
+  )
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -12,12 +49,10 @@ const App = () => {
   }, [])
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    <main>
+      {user && blogsList()}
+      {!user && userForm()}
+    </main>
   )
 }
 
