45f95c5a1345b2616635158a13070070a08c6b85
Lirmortoch <dima2111323@gmail.com>
2025-12-23 11:59:28 +0300

    add login functionality, add strict mode and some styles for labels

--- part 5/bloglist-frontend-main/src/App.jsx
+++ part 5/bloglist-frontend-main/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 import './App.css'
 
@@ -11,14 +12,29 @@ const App = () => {
   const usernameRef = useRef()
   const passwordRef = useRef()
 
-  function userFormSubmit(e) {
+  async function handleLogin(e) {
     e.preventDefault()
+
+    try {
+      const username = usernameRef.current.value
+      const password = passwordRef.current.value
+
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+
+      usernameRef.current.value = ''
+      passwordRef.current.value = ''
+    }
+    catch(error) {
+      console.log('wrong credentials', error)
+    }
   }
 
   const userForm = () => (
     <section>
       <h2>log in to application</h2>
-      <form onSubmit={userFormSubmit}>
+      <form onSubmit={handleLogin}>
         <fieldset>
           <label htmlFor='form-username'>username</label>
           <input type='text' id='form-username' name='form-username' ref={usernameRef} />
@@ -36,6 +52,7 @@ const App = () => {
   const blogsList = () => (
     <section>
       <h2>blogs</h2>
+      <p>{user.name}</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
