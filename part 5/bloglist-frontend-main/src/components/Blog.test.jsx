4e1c96ad2b575c42bbbfd0c0c779c85c0e2129b6
Lirmortoch <dima2111323@gmail.com>
2026-01-01 17:10:05 +0300

    did ex 5.15

---
+++ part 5/bloglist-frontend-main/src/components/Blog.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Blog from './Blog';
+import { expect } from 'vitest';
+
+describe('<Blog />', () => {
+  test('if "like" button was clicked twice, event handler will also be called twice', async () => {
+    const blogContent = {
+      title: 'React Test',
+      author: 'Dan Abramov',
+      url: 'https://react-test.com',
+    }
+
+    const user = userEvent.setup();
+
+    const updateLikeBlog = vi.fn();
+    const deleteBlog = vi.fn();
+
+    render(<Blog handleDeleteBlog={deleteBlog} handleLikeBlog={updateLikeBlog} blog={blogContent} />);
+
+    const likeBtn = screen.getByText('like');
+
+    await user.click(likeBtn);
+    await user.click(likeBtn);
+
+    expect(updateLikeBlog.mock.calls).toHaveLength(2);
+  });
+});
