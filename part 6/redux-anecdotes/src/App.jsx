913dd5db8c0ec95cea2fd081ff2aa11246817069
Lirmortoch <dima2111323@gmail.com>
2026-01-22 13:26:47 +0300

    did ex 6.12

--- part 6/redux-anecdotes/src/App.jsx
+++ part 6/redux-anecdotes/src/App.jsx
@@ -3,12 +3,14 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdotesFilter from './components/AnecdotesFilter'
 
 import './App.css'
+import Notification from './components/Notification'
 
 const App = () => {
   return (
     <main>
       <h1>Anecdotes!</h1>
-      
+      <Notification />
+
       <AnecdotesFilter />
       <AnecdoteList />
       <AnecdoteForm />
---
+++ part 6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'render here notification...',
+  reducers: {
+    
+  }
+})
+
+export default notificationSlice.reducer
