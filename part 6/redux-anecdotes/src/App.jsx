748e076272da3014909cd2f419c96044915ccf32
Lirmortoch <dima2111323@gmail.com>
2026-01-26 14:40:27 +0300

    implement very simple backend functionality and add service for getting all anecdotes

--- part 6/redux-anecdotes/src/App.jsx
+++ part 6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdotesFilter from './components/AnecdotesFilter'
 
 import './App.css'
 import Notification from './components/Notification'
+import anecdotesServices from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdotesServices.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <main>
       <h1>Anecdotes!</h1>
