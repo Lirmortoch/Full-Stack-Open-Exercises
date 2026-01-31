8a249f64d918bb748ad4845abab95edbe66c8062
Lirmortoch <dima2111323@gmail.com>
2026-01-31 19:27:47 +0300

    did exercise 6.20 (implement functionality for fetching data from server)

--- part 6/query-anecdotes/src/main.jsx
+++ part 6/query-anecdotes/src/main.jsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App'
 
+const QueryClient1 = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <QueryClientProvider client={QueryClient1}>
+    <App />
+  </QueryClientProvider>
 )
