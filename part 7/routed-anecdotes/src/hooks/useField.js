e33d6b78ea91738df0f91152454054f1609f563a
Lirmortoch <dima2111323@gmail.com>
2026-02-07 16:47:46 +0300

    did ex 7.5

--- part 7/routed-anecdotes/src/hooks/useField.js
+++ part 7/routed-anecdotes/src/hooks/useField.js
@@ -3,13 +3,17 @@ import { useState } from "react"
 export const useField = (name) => {
   const [value, setValue] = useState('')
 
-  const onChange = event => {
+  const onChange = (event) => {
     setValue(event.target.value)
+  }
+  const onReset = (event) => {
+    setValue('')
   }
 
   return {
     name,
     value,
     onChange,
+    onReset,
   }
 }
